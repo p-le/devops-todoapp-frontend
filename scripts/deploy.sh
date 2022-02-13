1d675b18be6d2f62c7439c03ab52ac103956c4f5
p-le <phu.le.dev@gmail.com>
2022-02-13 09:34:28 +0000

    update build script

---
+++ scripts/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -x
+
+IMAGE_NAME="todoapp-frontend"
+IMAGE_TAG="1.0.3"
+GCP_PROJECT_ID="phu-le-it"
+GCP_REGION="asia-northeast1"
+GCP_IMAGE_REPOSITORY="todoapp-repository"
+
+docker image build -t ${IMAGE_NAME}:dev .
+docker image tag ${IMAGE_NAME}:dev ${GCP_REGION}-docker.pkg.dev/${GCP_PROJECT_ID}/${GCP_IMAGE_REPOSITORY}/${IMAGE_NAME}:${IMAGE_TAG}
+docker image push ${GCP_REGION}-docker.pkg.dev/${GCP_PROJECT_ID}/${GCP_IMAGE_REPOSITORY}/${IMAGE_NAME}:${IMAGE_TAG}
